0e439317df924cbe6b945f6539925273d8a421c4
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-05-11 21:16:03 -0300

    Created basic MIDI interpretator

--- backend/src/controllers/AudioController.py
+++ backend/src/controllers/AudioController.py
@@ -1,6 +1,13 @@
 from src.validators import FileValidator
+from src.services.MidiService import MidiService
 
 def upload(file):
     response = FileValidator.validate(file)
 
-    print(response)
+    if not response:
+        midi_service = MidiService(file)
+        instruments = midi_service.get_instruments_name()
+
+        return { "instruments" : instruments }
+    
+    return response
