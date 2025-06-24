75975bb42eeccd94a145f405826fd3db75e6feac
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-06-23 21:10:58 -0300

    Cleaned codes

--- backend/src/services/MidiService.py
+++ backend/src/services/MidiService.py
@@ -13,67 +13,19 @@ class MidiService:
     def midi_data(self, value):
         self._midi_data = pretty_midi.PrettyMIDI(BytesIO(value.file.read()))
 
-    def get_instruments_name(self):
-        instruments = []
-        for item in self._midi_data.instruments:
-            instruments.append({
-                "name": item.name,
-                "program": int(item.program),
-                "isDrum": bool(item.is_drum),
-            })
-        return instruments
-
-    def extract_chords_by_guitar_type(self, chord_threshold=3):
-        general_midi_guitar_names = {
-            24: "Acoustic Guitar (nylon)",
-            25: "Acoustic Guitar (steel)",
-            26: "Electric Guitar (jazz)",
-            27: "Electric Guitar (clean)",
-            28: "Electric Guitar (muted)",
-            29: "Overdriven Guitar",
-            30: "Distortion Guitar",
-            31: "Guitar Harmonics"
-        }
-
-        result = []
-
-        for instrument in self._midi_data.instruments:
-            if not instrument.is_drum and 24 <= instrument.program <= 31:
-                program = instrument.program
-                notes_by_time = {}
-
-                for note in instrument.notes:
-                    notes_by_time.setdefault(note.start, []).append(note.pitch)
-
-                unique_chords = set()
-
-                for pitches in notes_by_time.values():
-                    if len(pitches) >= chord_threshold:
-                        chord = tuple(sorted(pretty_midi.note_number_to_name(p) for p in pitches))
-                        unique_chords.add(chord)
-
-                if unique_chords:
-                    result.append({
-                        "program": f"{program}",
-                        "title": general_midi_guitar_names.get(program, f"Program {program}"),
-                        "chords": [list(chord) for chord in sorted(unique_chords)]
-                    })
-
-        return result
-    
     def extract_chords(self, chord_threshold=3):
         chords = []
 
-        general_midi_guitar_names = {
-            24: "Acoustic Guitar (nylon)",
-            25: "Acoustic Guitar (steel)",
-            26: "Electric Guitar (jazz)",
-            27: "Electric Guitar (clean)",
-            28: "Electric Guitar (muted)",
-            29: "Overdriven Guitar",
-            30: "Distortion Guitar",
-            31: "Guitar Harmonics"
-        }
+        # general_midi_guitar_names = {
+        #     24: "Acoustic Guitar (nylon)",
+        #     25: "Acoustic Guitar (steel)",
+        #     26: "Electric Guitar (jazz)",
+        #     27: "Electric Guitar (clean)",
+        #     28: "Electric Guitar (muted)",
+        #     29: "Overdriven Guitar",
+        #     30: "Distortion Guitar",
+        #     31: "Guitar Harmonics"
+        # }
 
         for instrument in self._midi_data.instruments:
             if not instrument.is_drum and 24 <= instrument.program <= 31:
