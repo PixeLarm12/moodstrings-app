74a5314ae5ce9a3e5640bf6165b254e87abb60fb
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-06-23 21:18:53 -0300

    Cleaned

--- backend/src/validators/FileValidator.py
+++ backend/src/validators/FileValidator.py
@@ -8,5 +8,7 @@ def validate(file):
     if content_type != FileEnum.ContentTypes.AUDIO_MIDI.value:
         return { "errors" : "ContentType must be audio/mid type"}
     
-    if file_extension != FileEnum.Extensions.MID.value and file_extension != FileEnum.Extensions.MIDI.value:
+    if file_extension == FileEnum.Extensions.MID.value and file_extension == FileEnum.Extensions.MIDI.value:
         return { "errors" : "FileExtension must be .mid or .midi"}
+    
+    return False
