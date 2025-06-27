aee59690a549ed56df83f0c2d94eb1529bfb5173
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-06-27 01:06:40 -0300

    Huge fix

--- backend/src/validators/FileValidator.py
+++ backend/src/validators/FileValidator.py
@@ -5,10 +5,11 @@ def validate(file):
     content_type = file.content_type
     file_extension = FileUtil.getFileExtension(file)
 
-    if content_type != FileEnum.ContentTypes.AUDIO_MIDI.value:
-        return { "errors" : "ContentType must be audio/mid type"}
+    # NOT WORKING PROPERLY
+    # if content_type != FileEnum.ContentTypes.AUDIO_MIDI.value:
+    #     return { "errors" : "ContentType must be audio/mid type"}
     
-    if file_extension == FileEnum.Extensions.MID.value and file_extension == FileEnum.Extensions.MIDI.value:
-        return { "errors" : "FileExtension must be .mid or .midi"}
+    # if file_extension == FileEnum.Extensions.MID.value and file_extension == FileEnum.Extensions.MIDI.value:
+    #     return { "errors" : "FileExtension must be .mid or .midi"}
     
     return False
