95a8502e826ccce143c7be912a288c6ec30b6396
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-07-28 18:02:27 -0300

    Added mp3 transcription to .mid

--- backend/src/controllers/AudioController.py
+++ backend/src/controllers/AudioController.py
@@ -1,12 +1,17 @@
 from src.validators import FileValidator
 from src.services.MidiService import MidiService
 from src.services.IAService import IAService
+from src.services.AudioService import AudioService
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from src.services.AudioService import AudioService
+import io
 
 def upload(file):
     response = FileValidator.validate(file)
 
     if not response:
-        midi_service = MidiService(file)
+        midi_service = MidiService(file=file)
         ia_service = IAService()
 
         chordsPlayed = midi_service.extract_chords()
@@ -24,3 +29,17 @@ def upload(file):
         }
     
     return response
+
+async def test_mp3(file):
+    audio_service = AudioService(file)
+    midi_service = audio_service.transcribe_to_midi()
+
+    midi_io = io.BytesIO()
+    midi_service.midi_data.write(midi_io)
+    midi_io.seek(0)
+
+    return StreamingResponse(
+        midi_io,
+        media_type="audio/midi",
+        headers={"Content-Disposition": "attachment; filename=saida.mid"}
+    )
