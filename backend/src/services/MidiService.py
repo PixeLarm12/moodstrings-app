8b2a2eca688cdb9195cc7fa1852a92930a0b68af
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-08-22 16:40:06 -0300

    A lot of modifications

--- backend/src/services/MidiService.py
+++ backend/src/services/MidiService.py
@@ -1,6 +1,7 @@
 import pretty_midi
-from music21 import chord, pitch
-from src.utils.StringUtil import normalize_chord_name
+import tempfile
+from music21 import chord, pitch, converter, stream, note
+from src.utils.StringUtil import normalize_chord_name, simplify_chord_name
 from io import BytesIO
 
 class MidiService:
@@ -20,16 +21,18 @@ class MidiService:
     def midi_data(self, value):
         self._midi_data = pretty_midi.PrettyMIDI(BytesIO(value.file.read()))
 
-    def extract_chords(self, chord_threshold=3):
+    def extract_chords(self, chord_threshold=2):
         raw_chords = []
         named_chords = [];
 
         for instrument in self._midi_data.instruments:
-            if not instrument.is_drum and 24 <= instrument.program <= 31:
+            if not instrument.is_drum:
                 notes_by_time = {}
 
+                bucket_size = 0.25
                 for note in instrument.notes:
-                    notes_by_time.setdefault(note.start, []).append(note.pitch)
+                    bucket = round(note.start / bucket_size) * bucket_size
+                    notes_by_time.setdefault(bucket, []).append(note.pitch)
 
                 previous_chord = None
                 for time in sorted(notes_by_time.keys()):
@@ -39,24 +42,47 @@ class MidiService:
                         if item != previous_chord:
                             raw_chords.append(item)
 
+        prev = None
         for raw in raw_chords:
             note_names = raw.split("+")
             objChord = chord.Chord(note_names)
-            named_chords.append(normalize_chord_name(objChord.pitchedCommonName))
+            sc = simplify_chord_name(objChord.pitchedCommonName)
+            if sc and sc != prev:
+                named_chords.append(sc)
+                prev = sc
 
         return ' - '.join(named_chords)
 
-    def notes_to_chord_name(self, pitches: list[int]):
-        if not pitches or len(pitches) < 2:
-            return ""
+    # Used to created dataset. Probably, use primeFormString to define some chord is better than that name.
+    # def notes_to_chord_name(self, pitches: list[int]):
+    #     if not pitches or len(pitches) < 2:
+    #         return ""
 
-        try:
-            normalized_pitches = []
-            for p in pitches:
-                p_name = pretty_midi.note_number_to_name(p)[:-1]  
-                normalized_pitches.append(p_name)
+    #     try:
+    #         normalized_pitches = []
+    #         for p in pitches:
+    #             p_name = pretty_midi.note_number_to_name(p)[:-1]  
+    #             normalized_pitches.append(p_name)
 
-            c = chord.Chord(normalized_pitches)
-            return c.root()
-        except Exception:
-            return '+'.join([pretty_midi.note_number_to_name(p) for p in pitches])
+    #         c = chord.Chord(normalized_pitches)
+    #         return c.root()
+    #     except Exception:
+    #         return '+'.join([pretty_midi.note_number_to_name(p) for p in pitches])
+        
+    def extract_chords_new(self):
+        with tempfile.NamedTemporaryFile(suffix=".mid", delete=False) as tmp_midi:
+            self._midi_data.write(tmp_midi.name)
+            midi_path = tmp_midi.name
+
+        score = converter.parse(midi_path)
+        chords = score.chordify()
+
+        named_chords = []
+        prev = None
+        for c in chords.recurse().getElementsByClass('Chord'):
+            sc = simplify_chord_name(c.pitchedCommonName)
+            if sc and sc != prev:
+                named_chords.append(sc)
+                prev = sc
+
+        return " - ".join(named_chords)
