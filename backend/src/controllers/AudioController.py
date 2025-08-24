45de05fc8c262743b1e4b9daf3fd193c173c1bd4
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-08-24 19:11:49 -0300

    Create one single route to analyze both midi and mp3

--- backend/src/controllers/AudioController.py
+++ backend/src/controllers/AudioController.py
@@ -4,48 +4,55 @@ from src.services.IAService import IAService
 from src.services.AudioService import AudioService
 from fastapi.responses import StreamingResponse
 from src.services.AudioService import AudioService
+from src.utils import FileUtil
 import io
 
-def upload(file):
+def test_audio(file):
     response = FileValidator.validate(file)
 
     if not response:
-        midi_service = MidiService(file=file)
-        ia_service = IAService()
+        redirect_action = FileUtil.redirectByFileType(file)
+
+        if redirect_action == 'transcribe':
+            audio_service = AudioService(file)
+
+            midi_file = audio_service.create_midi_file()
+            
+            midi_service = MidiService(midi_data=midi_file)
+        else:
+            midi_service = MidiService(file=file)
+
+        # ia_service = IAService()
 
         chordsPlayedV1 = midi_service.extract_chords()
-        chordsPlayedV2 = midi_service.extract_chords_new()
+        # chordsPlayedV2 = midi_service.extract_chords_new()
         key_info = midi_service.find_estimate_key()
         tempo = midi_service.find_tempo()
 
-        if not chordsPlayedV1 and not chordsPlayedV2:
+        if not chordsPlayedV1:
             return {"error": "Unable to extract harmonic progression"}
-
-        # emotion, genre = ia_service.predict(chordsPlayed)
-
+        
         return {
             # "emotion": emotion,
             # "genre": genre, will be implemented in the future
             "chordProgressionV1": chordsPlayedV1,
-            "chordProgressionV2": chordsPlayedV2,
+            # "chordProgressionV2": chordsPlayedV2,
             "tempo": tempo,
             "key": key_info['key'],
             "mode": key_info['mode'],
             "tonic": key_info['tonic']
         }
-    
+
+        # midi_io = io.BytesIO()
+        # midi_service.midi_data.write(midi_io)
+        # midi_io.seek(0)
+
+        # return StreamingResponse(
+        #     midi_io,
+        #     media_type="audio/midi",
+        #     headers={"Content-Disposition": "attachment; filename=saida.mid"}
+        # )
+
     return response
 
-async def test_mp3(file):
-    audio_service = AudioService(file)
-    midi_service = audio_service.transcribe()
 
-    midi_io = io.BytesIO()
-    midi_service.midi_data.write(midi_io)
-    midi_io.seek(0)
-
-    return StreamingResponse(
-        midi_io,
-        media_type="audio/midi",
-        headers={"Content-Disposition": "attachment; filename=saida.mid"}
-    )
--- backend/src/services/AudioService.py
+++ backend/src/services/AudioService.py
@@ -1,31 +1,30 @@
 import io
 import tempfile
 from pydub import AudioSegment
-import numpy as np
 from basic_pitch.inference import predict
-from basic_pitch import ICASSP_2022_MODEL_PATH
-from src.services.MidiService import MidiService
+from pedalboard import Pedalboard, Compressor, HighpassFilter, LowpassFilter, Gain
+import soundfile as sf
+import noisereduce as nr
 
 class AudioService:
     def __init__(self, uploaded_file):
         self.uploaded_file = uploaded_file 
         self._midi_data = None
+        self._wav_path = None
 
-    def transcribe_to_midi(self):
-        mp3_bytes = self.uploaded_file.file.read()
+    def get_midi_data(self):
+        return self._midi_data
 
-        mp3_audio = AudioSegment.from_file(io.BytesIO(mp3_bytes), format="mp3")
-        mp3_audio = mp3_audio.set_frame_rate(16000).set_channels(1).set_sample_width(2)
+    def set_midi_data(self, midi_data):
+        self._midi_data = midi_data
 
-        with tempfile.NamedTemporaryFile(suffix=".wav", delete=True) as tmp_wav:
-            mp3_audio.export(tmp_wav.name, format="wav")
+    def get_wav_path(self):
+        return self._wav_path
 
-            output_dict, midi_data, note_events = predict(tmp_wav.name)
-            self._midi_data = midi_data
+    def set_wav_path(self, wav_path):
+        self._wav_path = wav_path
 
-        return MidiService(midi_data=self._midi_data)
-    
-    def transcribe(self):
+    def prepare_wav_file(self):
         mp3_bytes = self.uploaded_file.file.read()
 
         mp3_audio = AudioSegment.from_file(io.BytesIO(mp3_bytes), format="mp3")
@@ -33,9 +32,32 @@ class AudioService:
 
         with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmp_wav:
             mp3_audio.export(tmp_wav.name, format="wav")
-            wav_path = tmp_wav.name
 
-        model_output, midi_data, note_events = predict(wav_path)
+            self.set_wav_path(tmp_wav.name)
+    
+    def apply_filters(self):
+        samples, sr = sf.read(self.get_wav_path())
 
-        return MidiService(midi_data=midi_data)
+        samples = nr.reduce_noise(y=samples, sr=sr, prop_decrease=0.7)
 
+        board = Pedalboard([
+            # HighpassFilter(cutoff_frequency_hz=40.0),   
+            # LowpassFilter(cutoff_frequency_hz=16000.0), 
+            Gain(gain_db=3.0),                         
+            Compressor(threshold_db=-25, ratio=2.0),   
+        ])
+
+        samples = board(samples, sr)
+
+        sf.write(self.get_wav_path(), samples, sr)
+    
+    def create_midi_file(self):
+        self.prepare_wav_file()
+
+        self.apply_filters()
+
+        model_output, midi_data, note_events = predict(self.get_wav_path())
+
+        self.set_midi_data(midi_data)
+
+        return self.get_midi_data()
