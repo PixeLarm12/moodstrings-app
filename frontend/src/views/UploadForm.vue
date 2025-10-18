f46b898c12fd11373368a3db998a1df97f3f88f5
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2025-10-18 15:37:48 -0300

    New form organization

--- frontend/src/views/UploadForm.vue
+++ frontend/src/views/UploadForm.vue
@@ -1,13 +1,84 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-    <div class="w-full max-w-md text-center space-y-6">
+    <div class="w-full max-w-md md:max-w-2xl text-center space-y-6">
       <img src="/logo.png" alt="Logo" class="w-24 h-24 mx-auto rounded-full shadow-lg" />
 
       <h1 class="text-3xl font-bold">Upload de Arquivo</h1>
 
-      <ChordModal :show="showChordModal" :notes="modalNotes" :chord-name="modalChordName" @close="showChordModal = false" />
-      <AIModelModal :show="showAIModal" :evaluation="modalEvaluation" :model-name="modalModelName" @close="showAIModal = false" />
+      <Loading v-if="loading"></Loading>
+      <p v-else-if="message" class="mt-4 font-medium">
+        <span :class="success">{{ message }}</span>
+      </p>
 
+      <ChordModal :show="showChordModal" :notes="modalNotes" :chord-name="modalChordName" @close="showChordModal = false" />
+      <AIModelModal :show="showAIModal" :evaluation="modalEvaluation" :model-name="modalModelName" @close="showAIModal = false" />      
+
+      <!-- INFO CONTENT -->
+      <div v-if="chordProgression.length > 0" class="bg-gray-800 p-4 rounded-lg text-left space-y-1">
+        <h2 class="text-2xl font-bold mb-2">Principais informações</h2>
+
+        <span class="font-semibold text-blue-400">Sequência tocada: </span> 
+        
+        <p>
+          <span v-for="(info, index) in chordProgression" :key="index">
+            <button 
+              type="button" 
+              class="hover:text-blue-400 hover:cursor-pointer"
+              @click="openChordModal(info)"
+            >
+            {{ info.chord }}
+            </button> 
+            <span v-if="index < chordProgression.length - 1">, </span>
+          </span>
+        </p>
+
+        <hr class="my-4">
+
+        <span class="font-semibold text-blue-400">Emoção relacionada ao trecho: </span> 
+        
+        <p>
+          <ul v-for="(emotion, index) in emotions" :key="index">
+            <li class="list-none">
+              <button 
+                type="button" 
+                v-if="emotion.model_used != 'KNN'"
+                class="hover:text-blue-400 hover:cursor-pointer"
+                @click="openAIModelModal(emotion)"
+              >
+              {{ ++index }} - {{ emotion.content }} ({{ emotion.model_used }})
+              </button> 
+              <span v-else>{{ ++index }} - {{ emotion.content }} ({{ emotion.model_used }})</span>
+              <span v-if="index < chordProgression.length - 1">, </span>
+            </li>
+          </ul>
+        </p>  
+
+        <h2 class="text-2xl font-bold mt-8 mb-2">Secundárias</h2>
+
+        <p><span class="font-semibold text-blue-400">Tom:</span> {{ key }}</p>
+        <p><span class="font-semibold text-blue-400">Andamento:</span> {{ tempo }} BPM (<i>{{ tempoName }}</i>)</p>
+        <p><span class="font-semibold text-blue-400">Tônica:</span> {{ tonic }}</p>
+        <p><span class="font-semibold text-blue-400">Modo:</span> {{ mode }}</p>
+
+        <div class="flex justify-between my-4">
+          <button
+            type="submit"
+            class="py-2 w-[40%] bg-blue-600 rounded-lg font-semibold hover:bg-blue-700"
+          >
+            Ver escalas relativas
+          </button>
+
+          <button
+            type="submit"
+            class="py-2 w-[40%] bg-blue-600 rounded-lg font-semibold hover:bg-blue-700"
+          >
+            Download arquivo midi
+          </button>  
+        </div>
+      </div>
+      <!-- END INFO CONTENT -->
+      
+      <!-- FORM -->
       <form @submit.prevent="handleSubmit" class="space-y-4">
         <input
           type="file"
@@ -24,48 +95,7 @@
           Enviar
         </button>
       </form>
-
-      <Loading v-if="loading"></Loading>
-      <p v-else-if="message" class="mt-4 font-medium">
-        <span :class="success">{{ message }}</span>
-      </p>
-
-      <div v-if="chordProgression.length > 0" class="bg-gray-800 p-4 rounded-lg text-left space-y-1">
-        <p><span class="font-semibold text-blue-400">Tom:</span> {{ key }}</p>
-        <p>
-          <span class="font-semibold text-blue-400">Emoção: </span> 
-          <ul v-for="(emotion, index) in emotions" :key="index">
-            <li class="list-none">
-              <button 
-                type="button" 
-                v-if="emotion.model_used != 'KNN'"
-                class="hover:text-blue-400 hover:cursor-pointer"
-                @click="openAIModelModal(emotion)"
-              >
-              {{ ++index }} - {{ emotion.content }} ({{ emotion.model_used }})
-              </button> 
-              <span v-else>{{ ++index }} - {{ emotion.content }} ({{ emotion.model_used }})</span>
-              <span v-if="index < chordProgression.length - 1">, </span>
-            </li>
-          </ul>
-        </p>  
-        <p>
-          <span class="font-semibold text-blue-400">Progressão: </span> 
-          <span v-for="(info, index) in chordProgression" :key="index">
-            <button 
-              type="button" 
-              class="hover:text-blue-400 hover:cursor-pointer"
-              @click="openChordModal(info)"
-            >
-            {{ info.chord }}
-            </button> 
-            <span v-if="index < chordProgression.length - 1">, </span>
-          </span>
-        </p>  
-        <p><span class="font-semibold text-blue-400">Andamento:</span> {{ tempo }} BPM (<i>{{ tempoName }}</i>)</p>
-        <p><span class="font-semibold text-blue-400">Tônica:</span> {{ tonic }}</p>
-        <p><span class="font-semibold text-blue-400">Modo:</span> {{ mode }}</p>
-      </div>
+      <!-- END FORM -->
 
     </div>
   </div>
