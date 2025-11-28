3424c324f3174f415f8ce29e8ac18a263210d3e5
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2025-11-28 09:20:19 -0300

    added production host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -16,6 +14,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  server: {
+    allowedHosts: [
+      'moodstrings-front.onrender.com',
+    ],
   },
   watch: {
     usePolling: true,
