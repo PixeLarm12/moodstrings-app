d64ca2741d0a327a9969b3c2f14cb82930954054
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2025-11-28 09:42:48 -0300

    debug

--- backend/main.py
+++ backend/main.py
@@ -58,6 +58,13 @@ def test_evaluation(api_token: str):
             "message": "API Token is invalid."
         }
 
+@app.get("/get-api-key")
+def get_api_key(api_token: str):
+    return {
+        "API_KEY": API_KEY,
+        "api_token": api_token,
+    }
+
 
 # @app.get("/build-chunked-dataset")
 # def build_chunked_dataset():
